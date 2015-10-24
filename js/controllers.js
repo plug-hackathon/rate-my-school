2e45dfee8af2ac2ace18244d16c8b35d41f8df8f
rasjostrom <ras.sjostrom@hotmail.com>
2015-10-25 00:27:11 +0200

    Implement Parse Facebook login

--- js/controllers.js
+++ js/controllers.js
@@ -8,6 +8,49 @@ angular.module('rate-my-school.controllers', [])
     .controller('SchoolDetailsCtrl', function($scope, $routeParams, SchoolDetailsFactory){
         $scope.school = SchoolDetailsFactory.query({id:$routeParams.id});
 	Parse.initialize("3OWEYftfHGwWpvZ612fvlcxHef9ilMcNQhdgAJdj", "ScisaMB6uGwqLjesSyUlTYZfBnOkqyv69Uhveiss");
+
+	function statusChangeCallback(response) {
+	    if (response.status === 'connected') {
+		console.log('sent ratings');
+	    } else {
+		Parse.FacebookUtils.logIn(null, {
+		    success: function(user) {
+			if (!user.existed()) {
+			    alert("User signed up and logged in through Facebook!");
+			    console.log('sent ratings');
+			} else {
+			    alert("User logged in through Facebook!");
+			    console.log('sent ratings');
+			}
+		    },
+		    error: function(user, error) {
+			console.log("User cancelled the Facebook login or did not fully authorize.");
+		    }
+		});
+	    }
+	}
+
+	function checkLoginState() {
+	    FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	    });
+	}
+	
+	window.fbAsyncInit = function() {
+	    Parse.FacebookUtils.init({
+		appId      : '465399856995721',
+		xfbml      : true,
+		version    : 'v2.5'
+	    });
+	};
+
+	(function(d, s, id) {
+	    var js, fjs = d.getElementsByTagName(s)[0];
+	    if (d.getElementById(id)) return;
+	    js = d.createElement(s); js.id = id;
+	    js.src = "//connect.facebook.net/en_US/sdk.js";
+	    fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
 	
         $scope.maxRate = 10;
 
@@ -21,7 +64,7 @@ angular.module('rate-my-school.controllers', [])
         $scope.facilitiesRate = 5;
         $scope.foodRate = 5;
 
-	var testParse = function() {
-	    console.log('yey');
-        };
+	$scope.sendRatings = function() {
+	    checkLoginState();
+	}
     });
