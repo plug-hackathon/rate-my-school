78a975639d89c6dc3b4293757d46e8de41ebeb3c
Robin Karlsson <rk222ev@student.lnu.se>
2015-10-24 22:23:27 +0200

    Makes the ratingsform better

--- js/controllers.js
+++ js/controllers.js
@@ -6,7 +6,17 @@ angular.module('rate-my-app.controllers', [])
 
     .controller('SchoolDetailsCtrl', function($scope, $routeParams, SchoolDetailsFactory){
         $scope.school = SchoolDetailsFactory.query({id:$routeParams.id});
-        $scope.rate = 5;
-        $scope.max = 10;
-        $scope.isReadonly = false;
+
+        $scope.maxRate = 10;
+
+        $scope.teacherAverage = 5;
+        $scope.environmentAverage = 5;
+        $scope.facilitiesAverage = 5;
+        $scope.foodAverage = 5;
+
+        $scope.teachersRate = 5;
+        $scope.environmentRate = 5;
+        $scope.facilitiesRate = 5;
+        $scope.foodRate = 5;
+
     });
