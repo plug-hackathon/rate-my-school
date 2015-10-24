dd6c81ad84cfab35dfa7e1413a08ec68c22272f4
Robin Karlsson <rk222ev@student.lnu.se>
2015-10-24 16:49:04 +0200

    Adds list and details controllers and factories

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,9 @@
 angular.module('rate-my-app.controllers', [])
 
-    .controller('SchoolsCtrl', function($scope, jsonService){
-        $scope.schools = jsonService.query();
-        console.log($scope.schools);
+    .controller('SchoolsCtrl', function($scope, SchoolsFactory){
+        $scope.schools = SchoolsFactory.query();
     })
 
-    .controller('SchoolDetailsCtrl', function($scope, jsonService){
-        $scope.school = jsonService.query();
+    .controller('SchoolDetailsCtrl', function($scope, $routeParams, SchoolDetailsFactory){
+        $scope.school = SchoolDetailsFactory.query({id:$routeParams.id});
     });
