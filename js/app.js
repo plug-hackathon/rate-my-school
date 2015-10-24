bb1bca84af81e3339715b7f7fc75dfd468568b7a
John Herrlin <jherrlin@gmail.com>
2015-10-24 13:23:40 +0200

    Basic structure of angular app

---
+++ js/app.js
@@ -0,0 +1,17 @@
+angular.modeule('rate-my-school', [
+    'ngResource',
+    'ngRoute',
+])
+
+    .config(function($routeProvider){
+	$routeProvider
+	    .when('/', {
+	    	templateUrl: 'templates/home.html',
+		controller: 'HomeCtrl'
+	    })
+    })
+
+    .otherwise({
+	redirectTo: '/'
+    });
+	
