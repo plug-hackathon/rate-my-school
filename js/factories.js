1b1ceece1f325a6a37132f5f5d5db54a8e2d3e9a
Andreas Lundahl <mail@andreaslundahl.com>
2015-10-25 11:39:31 +0100

    Fetch all schools

--- js/factories.js
+++ js/factories.js
@@ -2,7 +2,7 @@
 angular.module('rate-my-school.factories', ['ngResource'])
 
     .factory('SchoolsFactory', function($resource) {
-        return $resource('https://api.parse.com/1/classes/schools', { }, {
+        return $resource('https://api.parse.com/1/classes/schools2', {limit: 1000, skip: "@skip"}, {
             query: {
                 method: 'GET',
                 isArray: false,
--- js/services.js
+++ js/services.js
@@ -1,9 +1,13 @@
 angular.module('rate-my-school.services', [])
 
-    .service('SchoolsService', function (SchoolsFactory){
-        return SchoolsFactory.query().$promise
-            .then(function (response){
-                return response.results;
-            });
+    .service('SchoolsService', function (SchoolsFactory, $q) {
+      return $q.all(_.range(7).map(function (offset) {
+        return SchoolsFactory.query({skip: offset * 1000}).$promise
+      })).then(function (ps) {
+        return ps.map(function (x) {
+          return x.results;
+        }).reduce(function (a, x) {
+          return a.concat(x);
+        }, []);
+      });
     });
-
